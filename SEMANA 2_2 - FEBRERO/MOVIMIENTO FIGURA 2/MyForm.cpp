6427913bd7f10b4146edb37f58e48e1daff0ca83
AndreZav1 <87845032+AndreZav1@users.noreply.github.com>
2022-02-10 23:54:46 -0500

    .

--- SEMANA 2_2 - FEBRERO/MOVIMIENTO FIGURA 2/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-
-using namespace CLASEMIERCOLES;
-
-void main() {
-
-	Application::Run(gcnew MyForm);
-}
-
--- SEMANA 2_2 - FEBRERO/MOVIMIENTO FIGURA/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-
-using namespace CLASEMIERCOLES2;
-
-void main() {
-
-	Application::Run(gcnew MyForm);
-}
-
